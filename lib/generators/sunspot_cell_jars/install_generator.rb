d09187dcb3547946b85de56f8b277b3d05b6ee6c
Greg Oleksiak <greg.oleksiak@gmail.com>
2014-07-03 09:40:47 -0400

    Update /lib for sunspot 2.1.x

--- lib/generators/sunspot_cell_jars/install_generator.rb
+++ lib/generators/sunspot_cell_jars/install_generator.rb
@@ -10,32 +10,38 @@ module SunspotCellJars
 
           Dir.mkdir("solr/lib") unless File.exists?("solr/lib")
 
-          files = %w{apache-mime4j-core-0.7.jar
-            apache-mime4j-dom-0.7.jar
-            apache-solr-cell-3.5-SNAPSHOT.jar
-            asm-3.1.jar
-            bcmail-jdk15-1.45.jar
-            bcprov-jdk15-1.45.jar
-            boilerpipe-1.1.0.jar
-            commons-compress-1.2.jar
-            dom4j-1.6.1.jar
-            fontbox-1.6.0.jar
-            icu4j-4_8_1_1.jar
-            jempbox-1.6.0.jar
-            metadata-extractor-2.4.0-beta-1.jar
-            netcdf-4.2-min.jar
-            pdfbox-1.6.0.jar
-            poi-3.8-beta4.jar
-            poi-ooxml-3.8-beta4.jar
-            poi-ooxml-schemas-3.8-beta4.jar
-            poi-scratchpad-3.8-beta4.jar
+          files = %w{
+            pdfbox-1.7.1.jar
+            apache-mime4j-core-0.7.2.jar  
+            poi-3.8.jar
+            apache-mime4j-dom-0.7.2.jar   
+            poi-ooxml-3.8.jar
+            bcmail-jdk15-1.45.jar         
+            poi-ooxml-schemas-3.8.jar
+            bcprov-jdk15-1.45.jar         
+            poi-scratchpad-3.8.jar
+            boilerpipe-1.1.0.jar          
             rome-0.9.jar
+            commons-compress-1.4.1.jar    
+            solr-cell-4.2.0.jar
+            dom4j-1.6.1.jar               
             tagsoup-1.2.1.jar
-            tika-core-0.10.jar
-            tika-parsers-0.10.jar
-            xercesImpl-2.8.1.jar
-            xml-apis-1.0.b2.jar
-            xmlbeans-2.3.0.jar}
+            fontbox-1.7.1.jar             
+            tika-core-1.3.jar
+            icu4j-49.1.jar                
+            tika-parsers-1.3.jar
+            isoparser-1.0-RC-1.jar        
+            vorbis-java-core-0.1.jar
+            jdom-1.0.jar                  
+            vorbis-java-tika-0.1.jar
+            jempbox-1.7.1.jar             
+            xercesImpl-2.9.1.jar
+            juniversalchardet-1.0.3.jar   
+            xmlbeans-2.3.0.jar
+            metadata-extractor-2.6.2.jar  
+            xz-1.0.jar
+            netcdf-4.2-min.jar
+          }
 
           files.each do |file|
             copy_file file, "solr/lib/#{file}"
